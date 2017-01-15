578246cb4e5181350fa9b840e7835fae1c5f0e02
sannexii <santhoshgs205@gmail.com>
2017-01-15 08:31:16 -0500

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,247 @@
+
+# coding: utf-8
+
+# In[1]:
+
+#
+print "dsfs
+
+
+# In[2]:
+
+# int, float
+
+
+# In[3]:
+
+a=10
+b=20
+
+
+# In[5]:
+
+print a+b
+print a-b
+print a*b
+print a/b
+print a%2
+print a**3
+
+
+# In[6]:
+
+print 10.0/20
+
+
+# In[7]:
+
+print 10/20.0
+
+
+# In[8]:
+
+a
+
+
+# In[9]:
+
+b
+
+
+# In[10]:
+
+print a.0/b
+
+
+# In[11]:
+
+print float(a)
+
+
+# In[12]:
+
+print float(a)/b
+
+
+# In[13]:
+
+print a/float(b)
+
+
+# In[14]:
+
+print float(a/b) # 0 0.0 0.5 none
+
+
+# In[15]:
+
+print 10+20*10/3
+
+
+# In[16]:
+
+a=10.23345
+
+
+# In[17]:
+
+int(a)
+
+
+# In[18]:
+
+a="python"
+print a
+
+
+# In[19]:
+
+a='python'
+print "a"
+print a
+
+
+# In[21]:
+
+product_name = "Product1"
+
+
+# In[23]:
+
+product$name = "product1"
+
+
+# In[24]:
+
+name1 = "dsfsd"
+
+
+# In[25]:
+
+1name = "dsfsdf"
+
+
+# In[26]:
+
+SfdfFRT = "fdsf"
+
+
+# In[27]:
+
+s1="str1"
+s2="str2"
+
+
+# In[30]:
+
+print s1+s2
+
+
+# In[31]:
+
+print s1-s2
+
+
+# In[32]:
+
+print s1*s2
+
+
+# In[34]:
+
+print s1/s2
+
+
+# In[35]:
+
+print "str1"+20
+
+
+# In[36]:
+
+price=24.4
+result = "product price: "+price
+
+
+# In[38]:
+
+price=24.4
+price_str = str(price)
+result = "product price: "+price_str
+print result
+
+
+# In[39]:
+
+s1="10"
+s2="20"
+print s1+s2
+
+
+# In[40]:
+
+print int(s1)+int(s2)
+
+
+# In[41]:
+
+s1="10.23"
+s2="20"
+print int(s1)
+
+
+# In[42]:
+
+print float(s1)
+
+
+# In[43]:
+
+s3 = float(s1)
+s4  =int(s3)
+
+
+# In[44]:
+
+s4
+
+
+# In[45]:
+
+int(10.23)
+
+
+# In[46]:
+
+int("10.23")
+
+
+# In[49]:
+
+int("123bacd")
+
+
+# In[50]:
+
+get_ipython().magic(u'pinfo int')
+
+
+# In[51]:
+
+int()
+
+
+# In[53]:
+
+int('abc',14)
+
+
+# In[54]:
+
+12*14**0+11*14**1+10*14**2
+
+
+# In[ ]:
+
+
+
